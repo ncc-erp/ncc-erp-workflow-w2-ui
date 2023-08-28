f07e0e18e10dc1f6245b92e1b2b133f10946c9d4
Hằng Cao <thuyhangfr01@gmail.com>
2023-08-28 13:56:25 +0700

    50971: decode jwt token and custom hook useIsAdmin

---
+++ src/hooks/useIsAdmin.ts
@@ -0,0 +1,17 @@
+import { ERole, LocalStorageKeys } from "common/enums";
+import { useMemo } from "react";
+import { parseJwt } from "utils/parseJwt";
+
+export function useIsAdmin() {
+    const token = localStorage.getItem(LocalStorageKeys.accessToken);
+    const decodedToken = useMemo(() => {
+        if (token) {
+            return parseJwt(token);
+        }
+        return null;
+    }, [token]);
+
+    const isAdmin = decodedToken ? decodedToken.role === ERole.Admin : false;
+
+    return isAdmin;
+}
