3a0fb21a49d343e9ed6c7435e31fa032b504718c
banhan14999 <huynhbanhan1491999@gmail.com>
2023-09-15 11:22:53 +0700

    Update Request Form

--- src/features/RequestTemplates/components/RequestTemplateModal.tsx
+++ src/features/RequestTemplates/components/RequestTemplateModal.tsx
@@ -7,30 +7,22 @@ import {
   ModalHeader,
   ModalOverlay,
 } from '@chakra-ui/react';
-import ChangeOfficeRequestForm from './forms/ChangeOfficeRequestForm';
-import { RequestTemplateWorkflow } from 'common/constants';
-import OfficeEquipmentRequestForm from './forms/OfficeEquipmentRequestForm';
-import WfhRequestForm from './forms/WfhRequestForm';
-import DeviceRequestForm from './forms/DeviceRequestForm';
+import { InputDefinition } from 'models/request';
+import RequestForm from './forms/RequestForm';
 interface RequestTemplateModalProps {
   isOpen: boolean;
   onClose: () => void;
   displayName?: string;
   requestId: string;
   workflow: string;
+  inputDefinition: InputDefinition | undefined;
 }
-const workflowComponents = {
-  [RequestTemplateWorkflow.CHANGE_OFFICE]: <ChangeOfficeRequestForm />,
-  [RequestTemplateWorkflow.OFFICE_EQUIPMENT]: <OfficeEquipmentRequestForm />,
-  [RequestTemplateWorkflow.DEVICE_REQUEST]: <DeviceRequestForm />,
-  [RequestTemplateWorkflow.WFH_REQUEST]: <WfhRequestForm />,
-};
 
 export const RequestTemplateModal = ({
   isOpen,
   onClose,
   displayName,
-  workflow,
+  inputDefinition,
 }: RequestTemplateModalProps) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -39,7 +31,9 @@ export const RequestTemplateModal = ({
         <ModalHeader fontSize="md">{displayName}</ModalHeader>
         <Divider></Divider>
         <ModalCloseButton />
-        <ModalBody>{workflowComponents[workflow]}</ModalBody>
+        <ModalBody>
+          <RequestForm inputDefinition={inputDefinition} />
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
