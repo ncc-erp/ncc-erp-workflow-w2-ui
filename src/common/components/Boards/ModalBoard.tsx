008cdd2408e19fd4b449379ad65ffacc9db48630
nhanhuynhba <135592986+nhanhuynhba@users.noreply.github.com>
2023-10-17 14:00:15 +0700

    Delete unnecessary lines

--- src/common/components/Boards/ModalBoard.tsx
+++ src/common/components/Boards/ModalBoard.tsx
@@ -78,10 +78,6 @@ const ModalBoard = (props: ModalBoardProps): JSX.Element => {
 
   const renderFormContent = (data: IDynamicFormProps[] | undefined) => {
     return data?.map(function (element, ind) {
-      if (element?.isFinalApprove) {
-        return;
-      }
-
       return (
         <FormControl key={ind}>
           <FormLabel fontSize={16} my={1} fontWeight="normal">
