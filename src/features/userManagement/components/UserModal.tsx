34845e0da160b06f2261473e336182649affdfc0
longvodinhhoang <long.vodinhhoang@ncc.asia>
2023-10-30 15:21:48 +0700

    53061: Allow close On Overlay Click in Edit User Modal

--- src/features/userManagement/components/UserModal.tsx
+++ src/features/userManagement/components/UserModal.tsx
@@ -53,7 +53,7 @@ export const UserModal = ({
   }
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
+    <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={true}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader fontSize="18px">{modalTitle} user</ModalHeader>
