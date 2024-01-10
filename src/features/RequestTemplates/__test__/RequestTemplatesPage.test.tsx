66244c621b3541e14b477d08b03154f4dc460178
Phan Kien <phankien110302@gmail.com>
2024-01-10 13:02:26 +0700

    modify test

--- src/features/RequestTemplates/__test__/RequestTemplatesPage.test.tsx
+++ src/features/RequestTemplates/__test__/RequestTemplatesPage.test.tsx
@@ -3,6 +3,7 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import RequestTemplates from '..';
 import userEvent from '@testing-library/user-event';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 jest.mock('../../../api/apiHooks/index', () => ({
   useAxios: jest.fn(),
@@ -76,7 +77,9 @@ describe('Request Template Page', () => {
     const { container } = render(
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
-          <RequestTemplates />
+          <Router>
+              <RequestTemplates />
+          </Router>
         </RecoilRoot>
       </QueryClientProvider>
     );
@@ -90,7 +93,9 @@ describe('Request Template Page', () => {
       render(
         <QueryClientProvider client={queryClient}>
           <RecoilRoot>
-            <RequestTemplates />
+            <Router>
+              <RequestTemplates />
+            </Router>
           </RecoilRoot>
         </QueryClientProvider>
       );
@@ -102,7 +107,7 @@ describe('Request Template Page', () => {
 
     it('should display the correct number of buttons on the screen', async () => {
       const buttonList = await screen.findAllByRole('button');
-      expect(buttonList).toHaveLength(totalCount + 3);
+      expect(buttonList).toHaveLength(totalCount + 4);
     });
 
     it('should handle selecting rows per page', async () => {
