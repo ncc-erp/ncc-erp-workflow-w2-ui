29a37b8cbb302e90c213e804d39a518ec6845a72
huynguyenanh1 <huy.nguyenanh@ncc.asia>
2024-07-16 14:14:26 +0700

    Bug/195 (#198)
    * set up story book
    
    * delete  mock data
    
    * change type timeout
    
    * delete useTimeout
    
    * Resolve conflicts storybook
    
    * Add storybook
    
    * Add storybook
    
    ---------
    
    Co-authored-by: ndcqn02 <ndcqn02@gmail.com>
    Co-authored-by: anhhuy0168 <anhhuy123zxc123@gmail.com>

---
+++ .storybook/preview.ts
@@ -0,0 +1,22 @@
+import { theme } from '../src/themes/theme';
+import type { Preview } from '@storybook/react';
+
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    chakra: {
+      theme,
+    },
+  },
+};
+
+export default preview;
+
+
