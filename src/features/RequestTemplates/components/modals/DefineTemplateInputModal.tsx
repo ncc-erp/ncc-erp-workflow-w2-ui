3ee0db3f838c67e5c72acd39e343fc1906174c89
Minh Tran Chi <141224728+minh-tranchi@users.noreply.github.com>
2024-09-05 08:47:46 +0700

    Add import/export function for workflow input (#230)
    * Add import/export function for workflow input
    
    * Fix code review
    
    * Fix code review
    
    * fix comment
    
    ---------
    
    Co-authored-by: duc.buiminh <duc.buiminh@ncc.asia>
    Co-authored-by: duy1111 <maixuanduy0605@gmail.com>

--- src/features/RequestTemplates/components/modals/DefineTemplateInputModal.tsx
+++ src/features/RequestTemplates/components/modals/DefineTemplateInputModal.tsx
@@ -1,5 +1,6 @@
 import {
   Divider,
+  Grid,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -10,11 +11,13 @@ import {
 import { InputDefinition } from 'models/request';
 import DefineInputForm from '../forms/DefineInputForm';
 import styles from '../style.module.scss';
+import ExportImportJson from '../ExportImportJson';
 interface DefineTemplateInputModalProps {
   isOpen: boolean;
   requestId: string;
   onClose: () => void;
   inputDefinition?: InputDefinition;
+  workflowName: string;
 }
 
 export const DefineTemplateInputModal = ({
@@ -22,12 +25,28 @@ export const DefineTemplateInputModal = ({
   onClose,
   inputDefinition,
   requestId,
+  workflowName,
 }: DefineTemplateInputModalProps) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={true}>
       <ModalOverlay />
       <ModalContent className={styles.customModal}>
-        <ModalHeader fontSize="md">Define Workflow Input</ModalHeader>
+        <Grid
+          templateColumns="1fr auto"
+          alignItems="center"
+          gap={4}
+          paddingRight={32}
+          marginTop={2}
+          marginBottom={2}
+        >
+          <ModalHeader fontSize="md">Define Workflow Input</ModalHeader>
+          <ExportImportJson
+            workflowName={workflowName}
+            requestId={requestId}
+            inputDefinition={inputDefinition}
+            onClose={onClose}
+          />
+        </Grid>
         <Divider></Divider>
         <ModalCloseButton />
         <ModalBody className={styles.customModalBody}>
