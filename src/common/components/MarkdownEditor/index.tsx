83dae427a1ff91b5ee77741a0fee853e44994b4f
huy.nguyenhuuminh <huy.nguyenhuuminh@ncc.asia>
2024-11-20 14:24:42 +0700

    feat(markdown-editor): [WIP] add button upload file and drag/drop file, paste image

--- src/common/components/MarkdownEditor/index.tsx
+++ src/common/components/MarkdownEditor/index.tsx
@@ -1,10 +1,18 @@
-import MDEditor, { MDEditorProps, RefMDEditor } from '@uiw/react-md-editor';
+import MDEditor, {
+  commands,
+  ICommand,
+  MDEditorProps,
+  RefMDEditor,
+} from '@uiw/react-md-editor';
 import {
   MarkdownPreviewProps,
   MarkdownPreviewRef,
 } from '@uiw/react-markdown-preview';
-import { RefAttributes } from 'react';
+import { RefAttributes, useState } from 'react';
 import { ForwardRefExoticComponent } from 'react';
+import { RiAttachment2, RiLoader4Line } from 'react-icons/ri';
+import './style.scss';
+
 export interface MarkdownEditorProps {
   value?: string;
   source?: string;
@@ -26,6 +34,102 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
   editorProps,
   previewProps,
 }) => {
+  const [uploading, setUploading] = useState(false);
+
+  const attachFileCommand: ICommand = {
+    name: 'image',
+    keyCommand: 'image',
+    buttonProps: { 'aria-label': 'Attach file', title: 'Attach file' },
+    // icon: uploading || true ? <RiLoader4Line className="animate-spin" /> : <RiAttachment2 />,
+    render: () => {
+      return (
+        <button type="button" className="button-upload" disabled={uploading}>
+          {uploading ? (
+            <RiLoader4Line className="animate-spin" />
+          ) : (
+            <RiAttachment2 />
+          )}
+        </button>
+      );
+    },
+
+    execute: async (state, api) => {
+      // OPEN my component.
+
+      // example
+      // const [image, setImaeg] = React.useState('');
+      // <ImageUploader value={image} onChange={setImage} />
+
+      // let image_url = await upload_image(image);
+      const image_url = 'imageUrl';
+
+      const modifyText = `![](${image_url})\n`;
+      api.replaceSelection(modifyText);
+    },
+  };
+
+  const fileUploader = async (file: File) => {
+    setUploading(true);
+    const imageURL = await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(URL.createObjectURL(file));
+        setUploading(false);
+      }, 3000)
+    );
+
+    return imageURL;
+  };
+
+  const onFilePasted = async (
+    dataTransfer: DataTransfer,
+    setMarkdown?: (value?: string) => void
+  ) => {
+    const files: File[] = [];
+    for (let index = 0; index < dataTransfer.items.length; index += 1) {
+      const file = dataTransfer.files.item(index);
+
+      if (file) {
+        files.push(file);
+      }
+    }
+
+    await Promise.all(
+      files.map(async (file) => {
+        const url = await fileUploader(file);
+        const isImage = file.type.startsWith('image/');
+        const insertedMarkdown = insertFileUrlToTextArea(
+          isImage ? `![](${url})` : `[${file.name}](${url})`
+        );
+        if (!insertedMarkdown) {
+          return;
+        }
+        setMarkdown?.(insertedMarkdown);
+      })
+    );
+  };
+
+  const insertFileUrlToTextArea = (insertString: string) => {
+    const textarea = document.querySelector('textarea');
+    if (!textarea) {
+      return null;
+    }
+
+    let sentence = textarea.value;
+    const len = sentence.length;
+    const pos = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+
+    const front = sentence.slice(0, pos);
+    const back = sentence.slice(pos, len);
+
+    sentence = front + insertString + back;
+
+    textarea.value = sentence;
+    textarea.selectionEnd = end + insertString.length;
+
+    return sentence;
+  };
+
   return type === 'preview' ? (
     <MDEditor.Markdown
       source={source}
@@ -37,6 +141,42 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
       value={value}
       onChange={onChange}
       preview="edit"
+      onPaste={async (event) => {
+        if (event.clipboardData.files.length > 0) {
+          event.preventDefault();
+          await onFilePasted(event.clipboardData, onChange);
+          return;
+        } else {
+          await onFilePasted(event.clipboardData, onChange);
+        }
+      }}
+      onDrop={async (event) => {
+        event.preventDefault();
+        await onFilePasted(event.dataTransfer, onChange);
+      }}
+      commands={[
+        commands.bold,
+        commands.italic,
+        commands.strikethrough,
+        commands.hr,
+        commands.title,
+        commands.divider,
+        commands.link,
+        commands.quote,
+        commands.code,
+        commands.codeBlock,
+        commands.comment,
+        commands.image,
+        attachFileCommand,
+        commands.table,
+        commands.divider,
+        commands.unorderedListCommand,
+        commands.orderedListCommand,
+        commands.checkedListCommand,
+        commands.divider,
+        commands.help,
+      ]}
+      minHeight={140}
       {...editorProps}
     />
   );
