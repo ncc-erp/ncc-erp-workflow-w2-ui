224207ad2397b49646ac7fc01cb5b5bf08d9100e
khanh.tranvan <khanh.tranvan@ncc.asia>
2024-12-04 10:17:43 +0700

    config env to deploy

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -8,21 +8,23 @@ import {
 } from 'react-router';
 const { VITE_API_BASE_URL, VITE_SENTRY_DSN, MODE } = import.meta.env;
 
-Sentry.init({
-  dsn: VITE_SENTRY_DSN,
-  integrations: [
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes,
-    }),
-    Sentry.replayIntegration(),
-  ],
-  tracePropagationTargets: [VITE_API_BASE_URL],
-  tracesSampleRate: MODE === 'production' ? 1.0 : 0.1,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  environment: MODE,
-});
+if (VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: VITE_SENTRY_DSN,
+    integrations: [
+      Sentry.reactRouterV6BrowserTracingIntegration({
+        useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      }),
+      Sentry.replayIntegration(),
+    ],
+    tracePropagationTargets: [VITE_API_BASE_URL],
+    tracesSampleRate: MODE === 'production' ? 1.0 : 0.1,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    environment: MODE,
+  });
+}
