510a571fc53b7276eddcf416bb148ae8c05e25cc
Cao Nam <caonam030402@gmail.com>
2025-06-18 17:03:13 +0700

    chore: comment out coveragePathIgnorePatterns and modulePathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -59,6 +59,6 @@ export default {
   },
   coverageReporters: ['html', ['text', { file: 'coverage.txt' }]],
   coverageDirectory: '<rootDir>/src/tests/coverage',
-  coveragePathIgnorePatterns: ['src/features/report/'],
-  modulePathIgnorePatterns: ['<rootDir>/src/features/report/'],
+  // coveragePathIgnorePatterns: ['src/features/report/'],
+  // modulePathIgnorePatterns: ['<rootDir>/src/features/report/'],
 };
